17106f7825ece4cb2730f0d9bc4551a17f18c9d8
paulreaver005 <43364091+paulreaver005@users.noreply.github.com>
2019-06-01 00:20:46 -0600

    Add files via upload

---
+++ Proyecto.py
@@ -0,0 +1,67 @@
+import random
+import math
+
+class Vehiculo():
+    def __init__(self):
+        print('this is a vehicle')
+
+    def fuerzaMotor(self):
+        torque = random.randint(0, 9)
+        return torque
+
+
+class Camion(Vehiculo):
+    def __init__(self):
+        self.distancia = 0
+        pass
+
+    def rendimiento(self):
+        avance = 2 * super().fuerzaMotor() + 1
+        return avance
+
+
+
+
+class Tractor(Vehiculo):
+    def __init__(self):
+        self.__distancia = 0
+        pass
+
+    def rendimiento(self):
+        avance = round(math.log2(2) * super().fuerzaMotor())
+        return avance
+
+
+class Sedan(Vehiculo):
+    def __init__(self):
+        pass
+
+    def rendimiento(self):
+        avance = 3 * (super().fuerzaMotor())**2
+        return avance
+
+
+class Bus(Vehiculo):
+    def __init__(self):
+        pass
+
+    def rendimiento(self):
+        avance = 5 * super().fuerzaMotor()
+        return avance
+
+
+camion = Camion()
+tractor = Tractor()
+sedan = Sedan()
+bus = Bus()
+
+
+#carrera
+
+carrera = {'camion': camion, 'tractor':tractor, 'sedan':sedan, 'bus':bus}
+
+
+for item in carrera.keys():
+    print(carrera[item].rendimiento())
+
+
